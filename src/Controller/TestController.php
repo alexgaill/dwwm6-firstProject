25a3cea2768b9b8743185363af53dd42acbc69f5
alexandre gaillot <gaillotalexandre@yahoo.fr>
2021-09-13 13:39:06 +0200

    hello & bye

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -1,11 +1,30 @@
 <?php
 namespace App\Controller;
 
-class TestController {
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+class TestController extends AbstractController{
+
+    /**
+     * @Route("/", name="hello")
+     *
+     * @return void
+     */
     public function hello()
     {
         $text = "<h1>Hello World!</h1>";
-        return $text;
+        return new Response($text);
+    }
+
+    /**
+    * @Route("/bye", name="bye")
+    *
+    * @return void
+    */
+    public function bye()
+    {
+        return $this->render("bye.html.twig");
     }
 }
